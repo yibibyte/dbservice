9049522e271a42be416f7bbdefb84051b82f7ed6
il <>
2024-01-15 09:30:00 +0300

    init

---
+++ src/main/java/ru/db/UserController.java
@@ -0,0 +1,29 @@
+package ru.db;
+
+import java.sql.SQLException;
+
+public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void createUser(String name, int age) throws SQLException {
+        User user = new User(0, name, age);
+        userRepository.insertUser(user);
+    }
+
+    public User getUserById(int id) throws SQLException {
+        return userRepository.getUserById(id);
+    }
+
+    public void updateUser(int id, String name, int age) throws SQLException {
+        User user = new User(id, name, age);
+        userRepository.updateUser(user);
+    }
+
+    public void deleteUser(int id) throws SQLException {
+        userRepository.deleteUser(id);
+    }
+}
---
+++ src/main/java/ru/db/UserRepository.java
@@ -0,0 +1,69 @@
+package ru.db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    private Connection connection;
+
+    public UserRepository() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydatabase", "postgres", "password");
+    }
+
+    public void insertUser(User user) throws SQLException {
+        String sql = "INSERT INTO users (name, age) VALUES (?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, user.getName());
+        statement.setInt(2, user.getAge());
+        statement.executeUpdate();
+    }
+
+    public User getUserById(int id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age")
+            );
+        } else {
+            return null;
+        }
+    }
+
+    public void updateUser(User user) throws SQLException {
+        String sql = "UPDATE users SET name = ?, age = ? WHERE id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, user.getName());
+        statement.setInt(2, user.getAge());
+        statement.setInt(3, user.getId());
+        statement.executeUpdate();
+    }
+
+    public void deleteUser(int id) throws SQLException {
+        String sql = "DELETE FROM users WHERE id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            users.add(new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age")
+            ));
+        }
+        return users;
+    }
+
+}
