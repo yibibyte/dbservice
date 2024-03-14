ceaf6511913f2576e627b10924436fc73e101811
il <>
2024-03-14 20:08:04 +0300

    add generic

---
+++ src/main/java/ru/db/generic/UsersPrint.java
@@ -0,0 +1,21 @@
+package ru.db.generic;
+
+public class UsersPrint {
+    public static void main(String[] args) {
+
+        Printer printer = new Printer();
+        String[] people = {"Tom", "Alice", "Sam", "Kate", "Bob", "Helen"};
+        Integer[] numbers = {23, 4, 5, 2, 13, 456, 4};
+        printer.<String>print(people);
+        printer.<Integer>print(numbers);
+    }
+}
+
+class Printer {
+
+    public <T> void print(T[] items) {
+        for (T item : items) {
+            System.out.println(item);
+        }
+    }
+}
